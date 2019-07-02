819e1f1b23b72ca20691c568cb9efef670292361
cmplx-xyttmt <archimedesisaac@gmail.com>
2019-07-02 23:31:10 +0300

    - Finish exercise 1.3.02- Star Field - Update source target to 1.8 (i.e to use java 1.8) and android minSdk to 24, target to 28.

--- 1.3.02-Exercise-DrawAStarfield/core/src/com/udacity/gamedev/starfield/Starfield.java
+++ 1.3.02-Exercise-DrawAStarfield/core/src/com/udacity/gamedev/starfield/Starfield.java
@@ -7,14 +7,13 @@ import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.utils.Array;
 
+import java.util.Random;
+
 /**
- * TODO: Start here!
  *
  * In this exercise we'll draw a star field of white points on a black background. The number of
  * points will be defined by a density parameter that states what proportion of the pixels should be
  * white.
- *
- * TODO: Run what you've got before making any changes
  *
  * One thing to note is we're using two new LibGDX classes, Array, and Vector2. We're using a custom
  * Array type so LibGDX can control the memory, and avoid unfortunate garbage collection events.
@@ -34,50 +33,49 @@ import com.badlogic.gdx.utils.Array;
 public class Starfield extends ApplicationAdapter {
 
     private static final float STAR_DENSITY = 0.01f;
-    ShapeRenderer shapeRenderer;
-    Array<Vector2> stars;
+    private Random random = new Random();
+    private ShapeRenderer shapeRenderer;
+    private Array<Vector2> stars;
 
     @Override
     public void create() {
-        // TODO: Initialize a shapeRenderer
+        shapeRenderer = new ShapeRenderer();
 
-        // TODO: Call initStars
-
+        initStars();
     }
 
-    public void initStars(float density) {
-        // TODO: Figure out how many stars to draw. You'll need the screen dimensions, which you can get using Gdx.graphics.getWidth() and Gdx.graphics.getHeight().
+    private void initStars() {
+        int width = Gdx.graphics.getWidth();
+        int height = Gdx.graphics.getHeight();
+        int numOfStars = (int)(width * height * Starfield.STAR_DENSITY);
 
-        // TODO: Create a new array of Vector2's to hold the star positions
+        stars = new Array<>(numOfStars);
 
-        // TODO: Use java.util.Random to fill the array of star positions
-
+        for (int i = 0; i < numOfStars; i++) {
+            stars.add(new Vector2(random.nextFloat()*width, random.nextFloat()*height));
+        }
     }
 
     @Override
     public void resize(int width, int height) {
-        initStars(STAR_DENSITY);
+        initStars();
         shapeRenderer = new ShapeRenderer();
     }
 
     @Override
     public void render() {
-        // TODO: Make the night sky black
-        Gdx.gl.glClearColor(1, 0, 0, 1);
+        Gdx.gl.glClearColor(0, 0, 0, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
-        // TODO: Begin a shapeRenderer batch using ShapeType.Point
+        shapeRenderer.begin(ShapeRenderer.ShapeType.Point);
 
-        // TODO: Loop through the star positions and use shapeRenderer to draw points
+        stars.forEach(point -> shapeRenderer.point(point.x, point.y, 0));
 
-        // TODO: End the shapeRenderer batch
-
+        shapeRenderer.end();
     }
 
     @Override
     public void dispose() {
-        // TODO: Dispose of our ShapeRenderer
-
-        super.dispose();
+        shapeRenderer.dispose();
     }
 }
